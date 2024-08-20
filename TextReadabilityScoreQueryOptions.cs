f91bcdd09c7139a479edf58483f3edcedf35134f
APIVerve Team <hello@apiverve.com>
2024-08-20 11:05:57 -0500

    Bug Fixes

---
+++ TextReadabilityScoreQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class TextReadabilityScoreQueryOptions {
+public string text { get; set; }
+}
+
+}
