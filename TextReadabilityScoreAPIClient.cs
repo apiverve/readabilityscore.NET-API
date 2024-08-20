b7716abdcc11496368b3695d404d57292f749d77
APIVerve Team <hello@apiverve.com>
2024-08-20 11:05:55 -0500

    Bug Fixes

--- TextReadabilityScoreAPIClient.cs
+++ TextReadabilityScoreAPIClient.cs
@@ -57,7 +57,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="callback"></param>
         /// <param name="options"></param>
-        public void ExecuteAsync(ExecuteAsyncCallback callback, readabilityscoreQueryOptions options = null)
+        public void ExecuteAsync(ExecuteAsyncCallback callback, TextReadabilityScoreQueryOptions options = null)
         {
             ThreadPool.QueueUserWorkItem(state =>
             {
@@ -71,7 +71,7 @@ namespace APIVerve
         /// </summary>
         /// <param name="options"></param>
         /// <returns></returns>
-        public ResponseObj Execute(readabilityscoreQueryOptions options = null)
+        public ResponseObj Execute(TextReadabilityScoreQueryOptions options = null)
         {
             try
             {
@@ -89,6 +89,7 @@ namespace APIVerve
 
                 var request = WebRequest.Create(url);
                 request.Headers["x-api-key"] = _apiKey;
+                request.Headers["auth-mode"] = "nuget-package";
                 request.Method = _method;
 
                 if (_method == "POST")
@@ -149,7 +150,7 @@ namespace APIVerve
             }
         }
 
-        private string constructURL(readabilityscoreQueryOptions options)
+        private string constructURL(TextReadabilityScoreQueryOptions options)
         {
             string url = _apiEndpoint;
 
